5ee0f776053c64abb0abbd59fb99bad790d2abe0
Chibuzor Daniel <uwezukwechibuzor@gmail.com>
2023-08-09 23:04:17 +0100

    Delete ts-client directory

--- ts-client/interchain_security.ccv.provider.v1/registry.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratedType } from "@cosmjs/proto-signing";
-import { MsgAssignConsumerKey } from "./types/interchain_security/ccv/provider/v1/tx";
-
-const msgTypes: Array<[string, GeneratedType]>  = [
-    ["/interchain_security.ccv.provider.v1.MsgAssignConsumerKey", MsgAssignConsumerKey],
-    
-];
-
-export { msgTypes }
